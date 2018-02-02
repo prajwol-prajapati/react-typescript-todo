f49608b7a1c202e6332fe08349706dd39bdf9bf7
prajwol-prajapati <prajwolprajapati123@gmail.com>
2018-02-02 15:07:46 +0545

    redux-persist problem

--- src/index.tsx
+++ src/index.tsx
@@ -10,12 +10,29 @@ import { logger } from 'redux-logger';
 import thunk from 'redux-thunk';
 // import AuthExample from './App';
 import MainWrapper from './Containers/MainWrapper';
+import { persistStore, persistReducer } from 'redux-persist';
+import * as storage from 'redux-persist/lib/storage';
+// import allReducer from './Reducers/allReducer';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 
-const store = createStore(allReducers, applyMiddleware(thunk, logger));
+const persistConfig = {
+	key: 'root',
+	storage: storage
+}
+
+console.log(storage.default.getItem);
+
+const persistedReducer = persistReducer(persistConfig, allReducers);
+
+const store = createStore(persistedReducer, applyMiddleware(thunk, logger));
+
+const persistor = persistStore(store);
 
 ReactDOM.render(
 	<Provider store={store}>
-		<MainWrapper />
+		<PersistGate loading={null} persistor={persistor}>
+			<MainWrapper />
+		</PersistGate>
 	</Provider>,
 	document.getElementById('root')
 );
